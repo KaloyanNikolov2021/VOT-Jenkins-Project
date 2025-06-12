c43ce37451da05f8d533db58aad2c173626ba809
Kaloyan Nikolov <123620829+KaloyanNikolov2021@users.noreply.github.com>
2025-06-12 22:22:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "this is my project for virtualization of cloud technologies using jensking and flash"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
